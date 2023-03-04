58a6dfd119b92e1eb995f5fd90249c55c1411647
softarf <98207887+softarf@users.noreply.github.com>
2023-03-04 23:34:05 +0500

    Вносит орфографические исправления. ДЗ 2, задача 2.
    И меняет регистр ключевых слов в файле task_3.2.2_create_db.sql.

--- task_3.2.2_create_db.sql
+++ task_3.2.2_create_db.sql
@@ -1,47 +1,47 @@
 
 CREATE TABLE IF NOT EXISTS genres (
-	genre_id sereal PRIMARY KEY,
-	name varchar(60) NOT NULL
+    genre_id SERIAL PRIMARY KEY,
+    name VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS singers (
-	singer_id sereal PRIMARY KEY,
-	nickname varchar(60) NOT NULL
+    singer_id SERIAL PRIMARY KEY,
+    nickname VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS genres_singers (
-	genre_singer_id sereal PRIMARY KEY,
-	genre_id integer NOT NULL REFERENCES genres,
-	singer_id integer NOT NULL REFERENCES singers
+    genre_singer_id SERIAL PRIMARY KEY,
+    genre_id INTEGER REFERENCES genres,
+    singer_id INTEGER REFERENCES singers
 );
 
 CREATE TABLE IF NOT EXISTS albums (
-	album_id sereal PRIMARY KEY,
-	name varchar(120) NOT NULL,
-	year integer CHECK (year > 1900)
+    album_id SERIAL PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    year INTEGER CHECK (year > 1900)
 );
 
 CREATE TABLE IF NOT EXISTS singers_albums (
-	singer_album_id sereal PRIMARY KEY,
-	singer_id integer NOT NULL REFERENCES singers,
-	album_id integer NOT NULL REFERENCES albums
+    singer_album_id SERIAL PRIMARY KEY,                   -- Первичный ключ, допускающий повторение комбинации. --
+    singer_id INTEGER REFERENCES singers,
+    album_id INTEGER REFERENCES albums
 );
 
 CREATE TABLE IF NOT EXISTS tracks (
-	track_id sereal PRIMARY KEY,
-	name varchar(120) NOT NULL,
-	duration integer CHECK (duration > 0),
-	album_id integer NOT NULL REFERENCES albums
+    track_id SERIAL PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    duration INTEGER CHECK (duration > 0),
+    album_id INTEGER REFERENCES albums
 );
 
 CREATE TABLE IF NOT EXISTS collections (
-	collection_id sereal PRIMARY KEY,
-	name varchar(120) NOT NULL,
-	year integer CHECK (year > 1900)
+    collection_id SERIAL PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    year INTEGER CHECK (year > 1900)
 );
 
 CREATE TABLE IF NOT EXISTS tracks_collections (
-	track_id integer REFERENCES tracks,
-	collection_id integer REFERENCES collections,
-	CONSTRAINT pk PRIMARY KEY (track_id, collection_id)
+    track_id INTEGER REFERENCES tracks,
+    collection_id INTEGER REFERENCES collections,
+    CONSTRAINT pk PRIMARY KEY (track_id, collection_id)   -- Составной первичный ключ, исключающий повторение комбинации. --
 );
--- task_3.2.3_company_structure.sql
+++ task_3.2.3_company_structure.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE IF NOT EXISTS employees (
-	employee_id sereal PRIMARY KEY,
-	name varchar(60) NOT NULL,
-	department varchar(120) NOT NULL,
-	boss integer REFERENCES employees (employee_id)
+	employee_id SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	department VARCHAR(120) NOT NULL,
+	boss INTEGER REFERENCES employees (employee_id)
 );
