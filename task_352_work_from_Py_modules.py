8d2c5f266b5113c0234dfeeebfac282ef8f82785
softarf <98207887+softarf@users.noreply.github.com>
2023-03-18 19:49:08 +0500

    Исправляет функцию поиска клиента. ДЗ 5, задача 1
    В модуле с функциями, в функции поиска клиента производит
    модефикацию select-запроса в коде. Сужает проверку при
    наличии конкретного спика клиентов.
    Modified:   task_352_work_from_Py_modules.py

--- task_352_work_from_Py_modules.py
+++ task_352_work_from_Py_modules.py
@@ -84,7 +84,6 @@ def delete_phone(cursor, customer_id, phone):
 
 def delete_client(cursor, customer_id):
     """6. Удаляет существующего клиента."""
-    pass
     cursor.execute("""
         SELECT phone_id FROM phones
         WHERE customer_id = %s;
@@ -103,84 +102,85 @@ def delete_client(cursor, customer_id):
     return customer_id
 
 
-def find_first_select(cursor, attribute_name, find_value):
-    """Предварительная выборка."""
-    stmt = sql.SQL("""
-        SELECT customer_id FROM customers
-        WHERE {attr} = %s;
-    """).format(attr=sql.Identifier(attribute_name), )
-    cursor.execute(stmt, (find_value, ))
-    find_list = [item[0] for item in cursor.fetchall()]
-    return find_list
+def find_by_phone(cursor, find_phone, customers_list=None):
+    """Выборка по телефону."""
+    phones_id_list = []
+    select_query = """
+            SELECT customer_id FROM phones
+                WHERE phone_number = %s;
+        """
+    query_items = (find_phone, )
+    if customers_list is not None and customers_list:
+        select_query = (select_query[: select_query.rindex(';')]
+                        + """ AND customer_id = %s;""")
+        for found_id in customers_list[:]:
+            cursor.execute(select_query, query_items + (found_id, ))
+            res = cursor.fetchone()
+            if res is not None:
+                phones_id_list.append(res[0])
+    else:
+        cursor.execute(select_query, query_items)
+        res = cursor.fetchone()
+        if res is not None:
+            phones_id_list.append(res[0])
+
+    found_id_list = phones_id_list[:]
+    return found_id_list
 
 
-def find_next_select(cursor, find_values, customers_ids_list):
-    """Последующая выборка."""
+def find_by_value(cursor, attribute_name, find_value, records_ids_list=None):
+    """Выборка по одному атрибуту."""
     find_list = []
-    for item in find_values:
-        find_list = []
-        for cus_id in customers_ids_list[:]:
-            stmt = sql.SQL("""
-                SELECT customer_id FROM customers
-                WHERE {attribute_name} = %s AND customer_id = %s;
-            """).format(attribute_name=sql.Identifier(item[0]), )
-            cursor.execute(stmt, (item[1], cus_id))
+    select_query = """
+            SELECT customer_id FROM customers
+                WHERE {select_attribute} = %s;
+            """
+    query_item = (find_value, )
+    if records_ids_list is not None and records_ids_list:
+        select_query = (select_query[: select_query.rindex(';')]
+                        + """ AND customer_id = %s;""")
+        stmt = sql.SQL(select_query
+                       ).format(select_attribute=sql.Identifier(attribute_name))
+        for record_id in records_ids_list[:]:
+            cursor.execute(stmt, query_item + (record_id, ))
             res = cursor.fetchone()
             if res is not None:
                 find_list.append(res[0])
-        customers_ids_list = find_list[:]
-    return find_list
-
-
-def find_phone_select(cursor, find_phone):
-    """Выборка по телефону."""
-    customer_id = []
-    cursor.execute("""
-        SELECT customer_id FROM phones
-            WHERE phone_number = %s;
-    """, (find_phone, ))
-    customer_id = [item[0] for item in cursor.fetchall()]
-    return customer_id
+    else:
+        stmt = sql.SQL(select_query
+                       ).format(select_attribute=sql.Identifier(attribute_name))
+        cursor.execute(stmt, query_item)
+        res = cursor.fetchone()
+        if res is not None:
+            find_list.append(res[0])
+    found_ids_list = find_list[:]
+    return found_ids_list
 
 
 def find_client(cursor, first_name=None, last_name=None, email=None, phone=None):
     """ 7. Находит клиента по его данным: имени, фамилии, email или телефону."""
-    item_found_list = []
-    first_parameter = ''
-    next_parameter = []
+    found_ids_list = []
     if first_name is not None:
-        first_parameter = first_name
-    if first_parameter != '':
-        item_found_list = find_first_select(cursor, 'first_name', first_parameter)
-        if item_found_list:
-            if last_name is not None:
-                next_parameter.append(('last_name', last_name))
-            if email is not None:
-                next_parameter.append(('email', email))
-            if next_parameter:
-                item_found_list = find_next_select(cursor, next_parameter, item_found_list)
+        found_ids_list = find_by_value(cursor, 'first_name', first_name)
+        if found_ids_list and last_name is not None:
+                found_ids_list = find_by_value(cursor, 'last_name', last_name, found_ids_list)
+                if found_ids_list and email is not None:
+                    found_ids_list = find_by_value(cursor, 'email', email, found_ids_list)
     else:
         if last_name is not None:
-            first_parameter = last_name
-        if first_parameter != '':
-            item_found_list = find_first_select(cursor, 'last_name', first_parameter)
-            if item_found_list:
-                if email is not None:
-                    next_parameter.append(email)
-                if next_parameter:
-                    item_found_list = find_next_select(cursor, next_parameter, item_found_list)
+            found_ids_list = find_by_value(cursor, 'last_name', last_name)
+            if found_ids_list and email is not None:
+                found_ids_list = find_by_value(cursor, 'email', email, found_ids_list)
         else:
             if email is not None:
-                first_parameter = email
-            if first_parameter != '':
-                item_found_list = find_first_select(cursor, 'email', first_parameter)
+                found_ids_list = find_by_value(cursor, 'email', email)
 
-    if (first_name is None and last_name is None and email is None) or item_found_list:
+    if (first_name is None and last_name is None and email is None) or found_ids_list:
         if phone is not None:
-            item_found_list = find_phone_select(cursor, phone)
+            found_ids_list = find_by_phone(cursor, phone, found_ids_list)
 
-    if len(item_found_list) == 0:
+    if len(found_ids_list) == 0:
         print("   Такого человека в Базе нет.")
-    elif len(item_found_list) > 1:
+    elif len(found_ids_list) > 1:
         print("   Слишком много совпадений. Уточните параметры поиска.")
-    return item_found_list, len(item_found_list)
+    return found_ids_list, len(found_ids_list) if found_ids_list else ([0], 0)
